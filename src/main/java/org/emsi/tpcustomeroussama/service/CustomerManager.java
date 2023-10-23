c0c01e5c1f1c4b0ab4c08c5bb5976644a548f881
oussamaym <oussamaelysm15@gmail.com>
2023-10-23 22:02:38 +0100

    Ajout CustomerManager

---
+++ src/main/java/org/emsi/tpcustomeroussama/service/CustomerManager.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.emsi.tpcustomeroussama.service;
+
+/**
+ *
+ * @author DELL
+ */
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import org.emsi.tpcustomeroussama.entity.Customer;
+
+@RequestScoped
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
